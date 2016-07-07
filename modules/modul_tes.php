846c4a0523bc0fa743274751b9a03611d81e8351
yovie <yoviesmanda@gmail.com>
2016-07-07 23:57:43 +0800

    test

--- modules/modul_tes.php
+++ modules/modul_tes.php
@@ -2,13 +2,48 @@
 	if(!defined( "APPLICATION_BASE" )) die( "Error" );
 
 	if(!login())
-			redirect( "login" );
+		redirect( "login" );
 
 	if( $post ) {
-		
-		redirect( "tes" );
+		$ck = select_one( "tes_jawaban", "tes_id=$post_tes_id AND pertanyaan_id=$post_pertanyaan_id");
+		if( empty($ck) ) {
+			insert( "tes_jawaban", array(
+				"tes_id"=>$post_tes_id,
+				"pertanyaan_id"=>$post_pertanyaan_id,
+				"jawaban_id"=>$post_jawaban_id,
+				"waktu"=>time()
+			));
+		} else {
+			update( "tes_jawaban", array(
+				"jawaban_id"=>$post_jawaban_id,
+				"waktu"=>time()
+			), "id=" . $ck->id );
+		}
+		update( "tes", array(
+			"selesai"=>time()
+		), "id=" . $post_tes_id );
+		die;
+	}
+
+	$test = select_one( "tes", "user_id=" . $user_session->id );
+	if( empty($test) ) {
+		$test_id = insert( "tes", array(
+			"user_id"=>$user_session->id,
+			"mulai"=>time()
+		));
+	} else {
+		update( "tes", array(
+			"mulai"=>time()
+		), "id=" . $test->id);
+		$test_id = $test->id;
 	}
 
 	$pertanyaan = select( "pertanyaan", "jenis='pertanyaan' ORDER BY 
 		standar_kompetensi_id ASC, kompetensi_id ASC, indikator_id ASC" );
 	$jawaban = select( "jawaban");
+
+	$old = select( "tes_jawaban", "tes_id=$test_id");
+	$sebelumnya = array();
+	foreach($old as $ol) {
+		$sebelumnya[ $ol->tes_id ][ $ol->pertanyaan_id ] = $ol;
+	}
--- templates/modul_tes.php
+++ templates/modul_tes.php
@@ -18,12 +18,16 @@
         <?php foreach($pertanyaan as $n=>$ta): ?>
             <label class="col-md-1"> <?php echo $n+1 ?> )</label>
             <label class="col-md-11" style="padding:0;"> <?php echo $ta->teks ?> </label>
-            <div class="col-md-11 col-md-offset-1" style="margin-bottom:10px;">
-                <form method="post" class="form-horizontal">
+            <div class="col-md-12" style="margin-bottom:10px;">
+                <form method="post" class="form-inline">
                 <?php foreach($jawaban as $ji=>$ja): ?>
-                    <label class="col-md-3" for="jawab<?php echo $ji ?>"> <?php echo $ja->teks ?> </label>
-                    <div class="col-md-1" id="jawab<?php echo $ji ?>">
-                        <input type="checkbox" name="jawab<?php echo $ji ?>" value="<?php echo $ja->id ?>" />
+                    <div class="form-group col-md-3">
+                        <input class="col-md-1" type="radio" id="jawab<?php echo $n.$ji ?>" name="jawab<?php echo $n ?>" data-pertanyaan_id="<?php echo $ta->id ?>" data-jawaban_id="<?php echo $ja->id ?>" onchange="send_answer(this)"  <?php 
+                            if( isset( $sebelumnya[$test_id][$ta->id] ) ) {
+                                echo $sebelumnya[$test_id][$ta->id]->jawaban_id==$ja->id ? " checked ":"";
+                            }
+                        ?> />
+                        <label class="col-md-11" for="jawab<?php echo $n.$ji ?>"> <?php echo $ja->teks ?> </label>
                     </div>
                 <?php endforeach; ?>
                 </form>
@@ -33,6 +37,15 @@
 
     <script type="text/javascript">
         
+        function send_answer(th) {
+            var tes_id = <?php echo $test_id; ?>;
+            var pertanyaan_id = $(th).data('pertanyaan_id');
+            var jawaban_id = $(th).data('jawaban_id');
+            $.post( "tes", {tes_id: tes_id, pertanyaan_id: pertanyaan_id, jawaban_id: jawaban_id}, function(res) {
+                console.log( "saved" );
+            } );
+        }
+
     </script>
 
     <style type="text/css">
