38add4451fc76d9f0cd10f195809cf56fa71e686
yovie <yoviesmanda@gmail.com>
2016-07-27 16:48:13 +0700

    komit

--- templates/modul_login.php
+++ templates/modul_login.php
@@ -1,77 +1,98 @@
 <html>
 	<head>
 		<!-- Custom Fonts -->
-    	<link href="./assets/js/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+		<link href="./assets/js/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 		<!-- Bootstrap Core CSS -->
-    	<link href="./assets/js/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+		<link href="./assets/js/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
 		<style type="text/css">
 			@import url(http://fonts.googleapis.com/css?family=Roboto:400);
 			body {
-			  background-color:#fff;
+			  background-color:#ddd;
 			  -webkit-font-smoothing: antialiased;
 			  font: normal 14px Roboto,arial,sans-serif;
 			}
 
 			.container {
-			    padding: 25px;
-			    position: fixed;
+				padding: 25px;
 			}
 
-			.form-login {
-			    background-color: #EDEDED;
-			    padding-top: 10px;
-			    padding-bottom: 20px;
-			    padding-left: 20px;
-			    padding-right: 20px;
-			    border-radius: 15px;
-			    border-color:#d2d2d2;
-			    border-width: 5px;
-			    box-shadow:0 1px 0 #cfcfcf;
-			}
-
-			h4 { 
-			 border:0 solid #fff; 
-			 border-bottom-width:1px;
-			 padding-bottom:10px;
-			 text-align: center;
-			}
-
-			.form-control {
-			    border-radius: 10px;
-			}
-
-			.wrapper {
-			    text-align: center;
+			.panel-heading{
+				text-align: center;
 			}
 
 		</style>
 	</head>
 	<body>
 		<div class="container">
-		    <div class="row">
-		        <div class="col-md-offset-5 col-md-3">
-		            <div class="form-login">
-		            <h4>Silakan login</h4>
-		            <form method="post">
-			            <input type="text" name="username" class="form-control input-sm chat-input" placeholder="username" />
-			            </br>
-			            <input type="password" name="password" class="form-control input-sm chat-input" placeholder="password" />
-			            </br>
-			            <div class="wrapper">
-			            <span class="group-btn">     
-			                <button class="btn btn-primary btn-md">login <i class="fa fa-sign-in"></i></button>
-			            </span>
-		            </form>
-		            </div>
-		            </div>
-		        
-		        </div>
-		    </div>
+			<div class="row">
+				<div class="col-md-8 col-md-offset-2">
+					<div class="panel panel-primary">
+						<div class="panel-heading">
+							<h3>Tes Kompetensi Karir</h3>
+							<h3>Peserta Didik</h3>
+							<h3>Sekolah Menengah Pertama</h3>
+						</div>
+						<div class="panel-body">
+							<p>Ketentuan pengisian kuisioner :</p>
+						</div>
+						<div class="panel-footer">
+							<center>
+								<button class="btn btn-success">Masuk</button> &nbsp; 
+								<button class="btn btn-success">Daftar</button>
+							</center>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+
+		<div id="form-login" class="modal fade" role="dialog">
+			<div class="modal-dialog">
+				<div class="modal-content">
+					<div class="modal-header">
+						<button type="button" class="close" data-dismiss="modal">&times;</button>
+						<h3 class="modal-title">Login</h3>
+					</div>
+					<div class="modal-body">
+						<form method="post">
+							<input type="text" name="username" class="form-control input-sm chat-input" placeholder="username" />
+							</br>
+							<input type="password" name="password" class="form-control input-sm chat-input" placeholder="password" />
+							</br>
+							<div class="wrapper">
+							<span class="group-btn">	 
+								<button class="btn btn-primary btn-md">login <i class="fa fa-sign-in"></i></button>
+							</span>
+						</form>
+					</div>
+					<div class="modal-footer">
+						<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div id="form-signup" class="modal fade" role="dialog">
+			<div class="modal-dialog">
+				<div class="modal-content">
+					<div class="modal-header">
+						<button type="button" class="close" data-dismiss="modal">&times;</button>
+						<h3 class="modal-title">Modal Header</h3>
+					</div>
+					<div class="modal-body">
+						<p>Some text in the modal.</p>
+					</div>
+					<div class="modal-footer">
+						<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+					</div>
+				</div>
+			</div>
 		</div>
 
 		<!-- jQuery -->
-    	<script src="./assets/js/jquery/dist/jquery.min.js"></script>
+		<script src="./assets/js/jquery/dist/jquery.min.js"></script>
 		<!-- Bootstrap Core JavaScript -->
-    	<script src="./assets/js/bootstrap/dist/js/bootstrap.min.js"></script>
+		<script src="./assets/js/bootstrap/dist/js/bootstrap.min.js"></script>
 	</body>
 </html>
